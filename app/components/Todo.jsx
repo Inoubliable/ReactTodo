83a1be2a0d28f98d5b958c180cba3495f3c3af41
Tim <tim.janzelj@gmail.com>
2017-05-11 18:00:43 +0200

    Add completed and created timestamps

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var moment = require('moment');
+
 import Checkbox from 'material-ui/Checkbox';
 
 class Todo extends React.Component {
@@ -8,7 +10,18 @@ class Todo extends React.Component {
 	}
 
 	render() {
-		var {id, text, completed} = this.props;
+		var {id, text, completed, createdAt, completedAt} = this.props;
+		var renderDate = () => {
+			var message = 'Created ';
+			var timestamp = createdAt;
+
+			if(completed) {
+				message = 'Completed ';
+				timestamp = completedAt;
+			}
+
+			return message + moment.unix(timestamp).format('MMM Do YYYY @ H:mm a');
+		};
 
 		return (
 			<div onClick={() => {
@@ -18,6 +31,7 @@ class Todo extends React.Component {
 					label={text}
 					checked={completed}
 				/>
+				<p>{renderDate()}</p>
 			</div>
 		)
 	}
