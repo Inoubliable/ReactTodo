8e13b29e495646299e710550a46f75be1cebbbe9
Tim <tim.janzelj@gmail.com>
2017-05-13 17:26:05 +0200

    Add todos reducer and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,6 @@
 var expect = require('expect');
 var df = require('deep-freeze-strict');
+var moment = require('moment');
 
 var reducers = require('reducers');
 
@@ -24,6 +25,37 @@ describe('Reducers', () => {
 			var res = reducers.searchTextReducer(df(false), df(action));
 
 			expect(res).toEqual(!action);
+		});
+	});
+
+	describe('todosReducer', () => {
+		it('should add new todo', () => {
+			var action = {
+				type: 'ADD_TODO',
+				text: 'test text'
+			};
+			var res = reducers.todosReducer(df(''), df(action));
+
+			expect(res.length).toEqual(1);
+			expect(res[0].text).toEqual(action.text);
+		});
+
+		it('should toggle todo', () => {
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: 1
+			};
+			var todos = [{
+				id: 1,
+				text: 'test text',
+				completed: false,
+				createdAt: moment().unix(),
+				completedAt: undefined
+			}];
+			var res = reducers.todosReducer(df(todos), df(action));
+
+			expect(res[0].completed).toEqual(!todos[0].completed);
+			expect(res[0].completedAt).toNotEqual(undefined);
 		});
 	});
 });
